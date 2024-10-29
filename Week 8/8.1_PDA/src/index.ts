24c32ec23305600694d0a27e8bda45e2f43f0ea0
0x1Shub <shubhamyeram13@gmail.com>
2024-10-29 12:47:16 +0530

    Completed: PDA

---
+++ Week 8/8.1_PDA/src/index.ts
@@ -0,0 +1,28 @@
+import {PublicKey} from '@solana/web3.js'
+import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID } from '@solana/spl-token';
+
+// Replace these with your actual values
+const userAddress = new PublicKey('5gjLjKtBhDxWL4nwGKprThQwyzzNZ7XNAVFcEtw3rD4i');
+const tokenMintAddress = new PublicKey('6NeR2StEEb6CP75Gsd7ydbiAkabdriMdixPmC2U9hcJs');
+
+// Derive the associated token address
+const getAssociatedTokenAddress = (mintAddress: any, ownerAddress: any) => {
+    return PublicKey.findProgramAddressSync(
+        [
+            ownerAddress.toBuffer(),
+            TOKEN_PROGRAM_ID.toBuffer(),
+            mintAddress.toBuffer(),
+        ],
+        ASSOCIATED_TOKEN_PROGRAM_ID
+    );
+};
+
+const [associatedTokenAddress, bump] = getAssociatedTokenAddress(tokenMintAddress, userAddress);
+console.log(`Associated Token Address: ${associatedTokenAddress.toBase58()}, bump: ${bump}`);
+
+const PDA = PublicKey.createProgramAddressSync(
+    [userAddress.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), tokenMintAddress.toBuffer(), Buffer.from([255])],
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+  );
+   
+  console.log(`PDA: ${PDA}`);

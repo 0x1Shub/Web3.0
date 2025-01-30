6e22c0e4609164130e5687fa91cbcf6362cc0346
0x1Shub <shubhamyeram13@gmail.com>
2025-01-30 13:04:54 +0530

    Client Side ETH

--- Week 21/21.1-Interacting With Existing Contract/src/App.tsx
+++ Week 21/21.1-Interacting With Existing Contract/src/App.tsx
@@ -4,6 +4,8 @@ import { config } from './config'
 import { Account } from './components/Account'
 import { WalletOptions } from './components/wallet-options'
 import "./App.css"
+import { TotalSupply } from './components/TotolSupply'
+import { AllowUSDT } from './components/AllowUSDT'
 
 const queryClient = new QueryClient()
 
@@ -18,9 +20,19 @@ function App() {
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}> 
         <ConnectWallet />
+        <TotalSupply />
+        <AllowUSDT />
+        <AccountConnected />
       </QueryClientProvider> 
     </WagmiProvider>
   )
+}
+
+function AccountConnected() {
+  const {address} = useAccount();
+  return <div>
+    {address ? "You are connected "+address : "You are not connected"}
+  </div>
 }
 
 export default App;

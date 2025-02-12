c8e58a194c3b0670fdf97d5f299745e74c2c2490
0x1Shub <shubhamyeram13@gmail.com>
2025-02-12 11:46:42 +0530

    Cyfrin Updraft - Solidity Data Types

---
+++ Cyfrin Updraft/02-Solidity Smart Contract Development/2.1-Simple Storage/01_Data_Types.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract BasicDataTypes {
+    // Basic Types : boolean, unit, int, address, bytes
+    bool isFavoriteNumber = true;
+    uint256 favoriteNumber = 10;
+    int256 num = -10;
+    string str = "Shubham";
+    address myAddress = 0xa27CEF8aF2B6575903b676e5644657FAe96F491F;
+    bytes32 favoriteBytes32 = "cat";
+}

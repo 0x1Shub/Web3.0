67a3f3501d3754debdb20303a43d387c226018b1
0x1Shub <shubhamyeram13@gmail.com>
2025-03-11 22:33:45 +0530

    Rust Data Types

--- Week 26/26.1-Rust Bootcamp/data_type/src/main.rs
+++ Week 26/26.1-Rust Bootcamp/data_type/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let ans: i32 = -1000;
+    println!("Hello, world!, {}", ans);
+
+    let even: bool = is_even(10);
+    println!("{}", even);
+
+    let name: String = String::from("Shubham");
+    println!("{}", name);
+}
+
+fn is_even(a: u32) -> bool {
+    return a % 2 == 0;
 }

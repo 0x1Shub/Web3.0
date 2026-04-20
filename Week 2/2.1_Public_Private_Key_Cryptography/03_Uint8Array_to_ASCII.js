516fbfd036f699ab4cb8b92d0c52a357a5ca56c3
0x1Shub <shubhamyeram13@gmail.com>
2026-04-20 20:21:55 +0530

    Week 3

--- Week 2/2.1_Public_Private_Key_Cryptography/03_Uint8Array_to_ASCII.js
+++ Week 2/2.1_Public_Private_Key_Cryptography/03_Uint8Array_to_ASCII.js
@@ -2,7 +2,15 @@ function bytesToAscii(byteArray) {
   return new TextDecoder().decode(byteArray);
 }
 
-// Example usage:
-const bytes = new Uint8Array([72, 101, 108, 108, 111]); // Corresponds to "Hello"
-const asciiString = bytesToAscii(bytes);
-console.log(asciiString); // Output: "Hello"
+const byteArray = new Uint8Array([72, 101, 108, 108, 111]); 
+const asciiString = bytesToAscii(byteArray);
+console.log(asciiString);
+
+
+function AsciiToByte(asciiString) {
+  return new TextEncoder().encode(asciiString);
+}
+
+const asciiString1 = 'Hello';
+const byteArray1 = AsciiToByte(asciiString1);
+console.log(byteArray1); 

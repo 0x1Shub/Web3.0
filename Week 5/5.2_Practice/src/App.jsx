2244c333f76d839101111f90a26dcf7093ba5dd5
0x1Shub <shubhamyeram13@gmail.com>
2026-05-17 22:43:18 +0530

    Week 5

--- Week 5/5.2_Practice/src/App.jsx
+++ Week 5/5.2_Practice/src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, {FC, useMemo} from 'react'
+import { ConnectionProvider, WalletProvider} from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { WalletModalProvider, WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import '@solana/wallet-adapter-react-ui/styles.css'
+
 
 const App = () => {
+
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(() => [
+    new UnsafeBurnerWalletAdapter(),
+  ], [network]);
+
   return (
-    <div>App</div>
+    <ConnectionProvider endpoint={endpoint}>
+    {/* // <ConnectionProvider endpoint={"https://api.devnet.solana.com"}> */}
+      <WalletProvider wallets={wallets} autoConnect>
+      {/* <WalletProvider wallets={[]} autoConnect> */}
+        <WalletModalProvider>
+            <div style={{display: 'flex' , justifyContent: 'space-between'}}>
+              <WalletMultiButton />
+              <WalletDisconnectButton />
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
 

83b66c55dff1fd0ce3aaac0e0c97db7f63b35d9c
0x1Shub <shubhamyeram13@gmail.com>
2026-06-04 22:46:04 +0530

    Week 6

--- Week 6/01_Token_Launchpad/01-token-launchpad-starter/src/App.jsx
+++ Week 6/01_Token_Launchpad/01-token-launchpad-starter/src/App.jsx
@@ -1,12 +1,41 @@
 import './App.css'
 import TokenLaunchpad from './components/TokenLaunchpad'
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { clusterApiUrl } from '@solana/web3.js'
+import { 
+  WalletModalProvider, 
+  WalletDisconnectButton, 
+  WalletMultiButton 
+} from '@solana/wallet-adapter-react-ui'
+import '@solana/wallet-adapter-react-ui/styles.css'
+import { useMemo } from 'react'
 
 function App() {
 
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+
+  const wallets = useMemo(() => 
+  [new UnsafeBurnerWalletAdapter()]
+  ,[])
+
+
   return (
-    <>
-      <TokenLaunchpad />
-    </>
+    <div className='w-full'>
+      <ConnectionProvider endpoint={'https://api.devnet.solana.com'}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <div style={{display: "flex", justifyContent: "space-between", padding: "20px 20px 0px 20px"}}>
+              <WalletMultiButton />
+              <WalletDisconnectButton />
+            </div>
+            <TokenLaunchpad />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
   )
 }
 
--- Week 6/01_Token_Launchpad/01-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ Week 6/01_Token_Launchpad/01-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,6 +1,37 @@
 import React from "react";
+import { createInitializeMint2Instruction, createMint, getMinimumBalanceForRentExemptMint } from '@solana/spl-token';
+import { Keypair, SystemProgram, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
+import { useWallet } from '@solana/wallet-adapter-react';
 
 const TokenLaunchpad = () => {
+
+    const wallet = useWallet()
+
+    const createToken = async () => {
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+        const keypair = Keypair.generate();
+
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: payer.publicKey,
+                newAccountPubkey: keypair.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId,
+            }),
+            createInitializeMint2Instruction(keypair.publicKey, decimals, mintAuthority, freezeAuthority, programId),
+        );
+
+        transaction.partialSign(keypair);
+
+        // await sendAndConfirmTransaction(connection, transaction, [payer, keypair], confirmOptions);
+        await wallet.signTransaction(transaction);
+        await wallet.sendTransaction(
+            transaction,
+            connection
+        );
+    }
+
     return (
         <div className="h-[100vh] w-full flex flex-col items-center justify-center gap-5">
             <h1 className="font-medium text-4xl mb-5">Solana Token Launchpad</h1>
